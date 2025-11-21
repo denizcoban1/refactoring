4b0bda01195f1b731a40555479a2d9f94cbae591
denizcoban1 <d.coban@mail.utoronto.ca>
2025-11-21 18:16:56 -0500

    Task 1.1 Fixed CheckStyle issues

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,25 @@
 package theater;
 
+/**
+ * Null values are not allowed.
+ * null not applicable
+ * */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
